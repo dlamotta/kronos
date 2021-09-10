f58737a941d639fb7fe29cd67879791b2e81762c
David La Motta <david@lamotta.co>
2021-09-10 09:27:20 -0400

    Some minor beautifying.

--- schema.lua
+++ schema.lua
@@ -8,32 +8,24 @@
 -- Author: David La Motta
 --
 local typedefs = require "kong.db.schema.typedefs"
-
 -- Grab pluginname from module name
-local plugin_name = ({...})[1]:match("^kong%.plugins%.([^%.]+)")
+local plugin_name = ({...})[1]:match("^kong%.plugins%.([^%.]+)") 
 
 local schema = {
   name = plugin_name,
   fields = {    
-    {
-      -- this plugin will only be applied to Consumers or Routes
-      consumer = typedefs.no_service
-    },
-    {
-      -- this plugin will only run within Nginx HTTP module
-      protocols = typedefs.protocols_http
-    },
-    {
-      config = {
+    -- this plugin will only be applied to Consumers or Routes
+    { consumer = typedefs.no_service },      
+    -- this plugin will only run within Nginx HTTP module 
+    { protocols = typedefs.protocols_http }, 
+    { config = {
         type = "record",
         fields = {
-          { 
-            strategy = { type = "string", required = true } 
-          },
+          { strategy = { type = "string", required = true } },
         },
       },
     },
-  },
+  }
 }
 
 return schema
